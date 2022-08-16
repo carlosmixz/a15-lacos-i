576eccf1e91784fd2315eddf0844d93728227844
carlosmixz <carlosmixz3@gmail.com>
2022-08-16 19:41:24 -0300

    Disneyworld

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,7 @@
 let Resposta = prompt("Você quer comer mais coxinha? S/N")
 let Coxinhas
 
-for(Coxinhas = 0; Resposta.toUpperCase() == "S"; Coxinhas += 2.5){
+for(Coxinhas = 0; Resposta.toUpperCase() == "S"; Coxinhas += 2.5) {
     Resposta = prompt("Você quer comer mais coxinha? S/N")
 }
 
